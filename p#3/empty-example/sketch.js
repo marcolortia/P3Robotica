3afcecedde05dd1bb455a136c8a4d5bf3475c55b
Víctor Valle <vihuvr0304@gmail.com>
2020-05-10 21:16:03 -0500

    subiendo material

---
+++ p#3/empty-example/sketch.js
@@ -0,0 +1,54 @@
+function setup() {
+  // put setup code here  
+  createCanvas(3500, 2200); 
+}
+
+function draw() {
+  // Verticales
+  //Punto de entrada de material
+  strokeWeight(4);
+  stroke('#000');
+  fill('#EEE');
+  rect(0, 1820, 140, 180);
+  // Anaquel 1   
+  stroke('#000');
+  fill('#EEE');
+  rect(610, 200, 240, 1500);
+  // Anaquel 2
+  stroke('#000');
+  fill('#EEE');
+  rect(970, 200, 240, 1500);
+  // Anaquel 3
+  stroke('#000');
+  fill('#EEE');
+  rect(1330, 200, 240, 1500);
+  // Anaquel 4
+  stroke('#000');
+  fill('#EEE');
+  rect(1690, 200, 240, 1500);
+  // Anaquel 5
+  stroke('#000');
+  fill('#EEE');
+  rect(2050, 200, 240, 1500);
+  // Horizontales
+  // Anaquel 6 
+  stroke('#000');
+  fill('#EEE');
+  rect(2450, 740, 750, 240);
+  // Anaquel 7
+  stroke('#000');
+  fill('#EEE');
+  rect(2450, 1100, 750, 240);
+  // Anaquel 8
+  stroke('#000');
+  fill('#EEE');
+  rect(2450, 1460, 750, 240);
+  //Punto de salida de material
+  stroke('#000');
+  fill('#EEE');
+  rect(3320, 300, 180, 140);
+   //Robot   
+   fill('#000');
+   rect(410, 2050, 100, 140);
+
+}
